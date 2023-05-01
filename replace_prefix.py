0456afc994a7d736f782b01081fefc2b1f0060c0
masuda <masuda.tadashi.fm@un.tsukuba.ac.jp>
2023-05-01 13:24:02 +0900

    Save the status and rename the project, 2023/5/1

--- replace_prefix.py
+++ replace_prefix.py
@@ -1,12 +1,32 @@
 import re
+
+import rdflib.plugins.sparql.parser
 from rdflib.plugins.sparql.parser import parseQuery
+from rdflib.plugins.sparql.algebra import translatePrologue
+from rdflib.plugins.sparql.processor import prepareQuery
+from rdflib import Graph
+# from rdflib.tools.pathutils import guessFormat
 
 
 def replace_prefix(sparql_query):
     return_query = sparql_query
+
+    # g = Graph()
+    # g.parse(data=sparql_query, format='sparql')
+
+    prepared_query = prepareQuery(sparql_query, initNs={})
+    pq_algebra = prepared_query.algebra
+    pqa_str = str(pq_algebra)
+    zzz = pq_algebra.values()
+
     parsed_query = parseQuery(sparql_query)
+    # ttt = parsed_query[0].
+    tree = rdflib.plugins.sparql.parserutils.prettify_parsetree(parsed_query)
+    xxx = rdflib.plugins.sparql.parser.expandTriples(parsed_query)
+    prologue = translatePrologue(parsed_query, base=None)
     prefixes = {}
-    prefix_items = parsed_query.as_list()[0]
+    # prefix_items = parsed_query.as_list()[0]
+    prefix_items = parsed_query[0]
     for decl in prefix_items:
         prefix_string = decl['prefix']
         iri_string = decl['iri']
--- replace_test.py
+++ replace_test.py
@@ -1,4 +1,8 @@
+import ast
+import json
 import re
+
+import rdflib.plugins.sparql.parser
 
 sparql_string = 'SELECT ?s  WHERE {?s <http://foaf/friend> ?o ; <http://foaf/name> ?name.} LIMIT 10'
 
@@ -7,5 +11,41 @@ str_dict = {'<http://foaf/friend>': '<http://example.com/friend>', '<http://foaf
 matches = re.findall(r'<(.*?)>', sparql_string)
 for match in matches:
     sparql_string = sparql_string.replace(f'<{match}>', str_dict[f'<{match}>'])
-print(sparql_string)
+# print(sparql_string)
 pass
+
+query_input = """
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX ex: <http://example.com/ontology/>
+PREFIX country: <http://example.com/predicate/country>
+PREFIX country_name: <http://example.com/predicate/country_name>
+PREFIX country_comment: <http://example.com/predicate/country_comment>
+
+SELECT ?s ?name ?cname
+WHERE {
+    ?s rdf:type ex:Hotel.
+    ?s rdf:label ?name.
+    ?s country: ?c_id.
+    ?c_id country_name: ?cname.
+}
+"""
+prefix = 'rdf'
+uri = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#'
+pattern = re.compile(fr'{prefix}:(\w*)')
+query_output = pattern.sub(f'<{uri}\\1>', query_input)  # \\1 is (\w*)
+pattern = re.compile(fr'{prefix}:(..)([a-z:A-Z]*)')
+query_output = pattern.sub(f'<{uri}\\2\\1>', query_input)  # \\1 is (\w*)
+# print(query_output)
+
+parsed_query = rdflib.plugins.sparql.parser.parseQuery(query_input)
+xxx = str(parsed_query)
+print(xxx)
+# yyy = ast.literal_eval(xxx)
+print(json.loads(xxx))
+for p in parsed_query[0]:
+    print(p.name)
+print(parsed_query[1].name)
+print(parsed_query[1].values())
+for x in parsed_query[1].values():
+    print(x)
+
